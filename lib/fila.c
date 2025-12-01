0e6e933b6e670a7bdcf55c4b7f8bd855d19603d8
Arthur Ramos Vieira <arthurramos7890@gmail.com>
2025-12-01 09:51:24 -0300

    comentarios fila

--- lib/fila.c
+++ lib/fila.c
@@ -1,9 +1,20 @@
 #include "lib/fila.h"
 
+
+/*
+    filaInicializar
+    parêmetros: Nenhum
+    retorno: Uma fila inicializada com NULL
+*/
 Fila* filaInicializar(){
     return NULL;
 }
 
+/*
+    filaFrente
+    parêmetros: Um ponteiro duplo para fila
+    retorno: nenhum, printa na tela a frente da fila caso exista
+*/
 void filaFrente(Fila** f){
     if(*f == NULL){
         printf("Fila Vazia\n");
@@ -12,6 +23,12 @@ void filaFrente(Fila** f){
     }
 }
 
+
+/*
+    filaEnfileira
+    parâmetro: Ponteiro duplo para fila e um inteiro para enfileirar
+    retorno: 0 se o enfileiramento der errado, 1 se der certo
+*/
 int filaEnfileira(Fila** f, int valor){
     Fila* novo = (Fila*)malloc(sizeof(Fila));
     if(novo == NULL) return 0;
@@ -33,6 +50,11 @@ int filaEnfileira(Fila** f, int valor){
     return 1;
 }
 
+/*
+    filaDesenfileira
+    parâmetro: Ponteiro duplo para fila
+    retorno: 0 se der errado, 1 se der certo
+*/
 int filaDesenfileira(Fila** f){
     if(*f == NULL){
         return 0;
@@ -44,6 +66,11 @@ int filaDesenfileira(Fila** f){
     return 1;
 }
 
+/*
+    filaLiberar
+    parâmetro: Ponteiro duplo para fila
+    retorno: nenhum, porém, libera memória da fila
+*/
 void filaLiberar(Fila** f){
     Fila* atual = *f;
     while(atual != NULL){
@@ -54,6 +81,11 @@ void filaLiberar(Fila** f){
     *f = NULL;
 }
 
+/*
+    filaVazia
+    parâmetro: Ponteiro para fila
+    retorno: 1 se a fila estiver vazia, 0 se não estiver
+*/
 int filaVazia(Fila* f){
     return (f == NULL);
 }
