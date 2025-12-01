2c44bc47747e3e075d61b7938ab004ecb23e99ab
Arthur Ramos Vieira <arthurramos7890@gmail.com>
2025-12-01 10:02:00 -0300

    comentarios pilha

--- lib/pilha.c
+++ lib/pilha.c
@@ -1,9 +1,19 @@
 #include "lib/pilha.h"
 
+/*
+    pilhaInicializar
+    parâmetro: Nenhum
+    retorno: Pilha inicializada com NULL
+*/
 Pilha* pilhaInicializar() {
     return NULL;
 }
 
+/*
+    pilhaPush
+    parâmetros: Ponteiro duplo para pilha e valor inteiro a ser inserido
+    retorno: 1 se for bem sucedido, 0 se não for
+*/
 int pilhaPush(Pilha** p, int valor){
     Pilha* novo = (Pilha*)malloc(sizeof(Pilha));
     if(novo == NULL){
@@ -17,6 +27,11 @@ int pilhaPush(Pilha** p, int valor){
     return 1;
 }
 
+/*
+    pilhaPop
+    parâmetros: Ponteiro duplo para pilha
+    retorno: 1 se for bem sucedido, 0 se não for
+*/
 int pilhaPop(Pilha** p){
     if(*p == NULL){
         return 0;
@@ -29,6 +44,11 @@ int pilhaPop(Pilha** p){
     return 1;
 }
 
+/*
+    pilhaTopo
+    parçametro: Ponteiro duplo para pilha
+    retorno: Nenhum, printa o topo da Pilha no terminal
+*/
 void pilhaTopo(Pilha** p){
     if(*p == NULL){
         printf("Pilha vazia\n");
@@ -37,6 +57,11 @@ void pilhaTopo(Pilha** p){
     printf("Topo: %d\n", (*p)->numero);
 }
 
+/*
+    pilhaLiberar
+    parâmetro: Ponteiro duplo para pilha
+    retorno: Nanhum, porém libera a memória da pilha alocada
+*/
 void pilhaLiberar(Pilha** p){
     while(*p != NULL){
         Pilha* tmp = *p;
