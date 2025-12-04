7bdab31da6d3f791e1bbd8b1e0720a88bace55fb
raffscardoso <raffs.cardoso15@gmail.com>
2025-12-04 12:00:49 -0300

    remocao arquivo log

--- lib/arvore.h
+++ lib/arvore.h
@@ -1,29 +1,26 @@
 #ifndef ARVORE_H
 #define ARVORE_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef enum {
-    NORMAL = 0,
-    RECURSO = 1,
-    BATALHA = 2,
+  NORMAL = 0,
+  RECURSO = 1,
+  BATALHA = 2,
 } TipoTerritorio;
 
 typedef struct no {
-    int index;              // índice do nó (1 a 15)
-    int nivel;              // nível (0 a 3)
-    char nome[50];
-    TipoTerritorio tipo;
-    int ocupado;            // 0=livre, 1=jog1, 2=jog2
-    struct no *pai, *esq, *dir;
+  int index; // índice do nó (1 a 15)
+  int nivel; // nível (0 a 3)
+  char nome[50];
+  TipoTerritorio tipo;
+  int ocupado; // 0=livre, 1=jog1, 2=jog2
+  struct no *pai, *esq, *dir;
 } No;
 
-
-No* criarNo(int index, int nivel, const char* nome, No* pai);
-No* arvoreCriar();
-No* arvoreBuscarPorIndice(No* raiz, int index);
-
+No *criarNo(int index, int nivel, const char *nome, No *pai);
+No *arvoreCriar();
+No *arvoreBuscarPorIndice(No *raiz, int index);
 
 #endif
